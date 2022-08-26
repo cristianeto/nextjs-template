d184a67c64f1c976a88a8f8fb3a632d9baeec9c1
Cristian Guaman <cristian.guaman@icloud.com>
2022-08-26 01:25:08 -0500

    chore: /pages folder was moved inside src/pages

--- src/pages/index.spec.tsx
+++ src/pages/index.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Home from 'pages';
+import Home from './index';
 
 describe('Index Page', () => {
   it('should create index page', () => {
