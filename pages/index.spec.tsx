7e78c1ff6729d00df2c3cb193f644a2f741a0f54
Cristian Guaman <cristian.guaman@icloud.com>
2022-08-26 01:05:58 -0500

    chore: config jest and react testing library

---
+++ pages/index.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import Home from 'pages';
+
+describe('Index Page', () => {
+  it('should create index page', () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('heading', { name: /hello world/i });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
